6720471dbecddd31cd3a8eb1d22bfb827a1a15bf
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2018-12-05 10:35:29 +0200

    35 Blocking and Non-Blocking code

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer(function(req, res) {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
@@ -10,6 +12,22 @@ const server = http.createServer(function(req, res) {
     );
     res.write("<html>");
     return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, err => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
