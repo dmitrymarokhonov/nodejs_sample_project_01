5bbd727f0d3ee868a21896594ad363f93ee64436
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2018-12-06 23:55:51 +0200

    63 Parsing incoming request, body-parser package added

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use("/", (req, res, next) => {
-  console.log("First always");
   next();
 });
 app.use("/add-product", (req, res, next) => {
-  console.log("In /add-product middleware!");
-  res.send("<h1>Add Product Page</h1>");
+  res.send(`<form action="/product" method="POST">
+  <input type="text" name="title" />
+  <button type="submit">Add Product</button>
+</form>`);
 });
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+})
+
 app.use("/", (req, res, next) => {
-  console.log("In /root middleware!");
   res.send("<h1>Hello from exppress!</h1>");
 });
 
