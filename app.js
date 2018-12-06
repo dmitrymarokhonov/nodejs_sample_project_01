12a763fef4fbd269b2a5f89bcee32d98580be07f
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2018-12-06 14:53:18 +0200

    62 completed

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const http = require("http");
-const routes = require("./routes");
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+const express = require("express");
 
-server.listen(3000);
+const app = express();
+
+app.use("/", (req, res, next) => {
+  console.log("First always");
+  next();
+});
+app.use("/add-product", (req, res, next) => {
+  console.log("In /add-product middleware!");
+  res.send("<h1>Add Product Page</h1>");
+});
+app.use("/", (req, res, next) => {
+  console.log("In /root middleware!");
+  res.send("<h1>Hello from exppress!</h1>");
+});
+
+app.listen(3000);
