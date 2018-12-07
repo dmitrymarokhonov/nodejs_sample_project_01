e61ccb1a6926d029918703b5b40791a8bac9bc44
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2018-12-07 15:34:31 +0200

    72 completed (Express router, 404 page, HTML pages with CSS)

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/", (req, res, next) => {
-  next();
-});
-app.use("/add-product", (req, res, next) => {
-  res.send(`<form action="/product" method="POST">
-  <input type="text" name="title" />
-  <button type="submit">Add Product</button>
-</form>`);
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-})
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from exppress!</h1>");
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
 });
 
 app.listen(3000);
