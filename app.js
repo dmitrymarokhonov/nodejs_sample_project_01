41e8c7fc99e22b37fa326721ad60b013e642bf4f
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2019-01-27 01:52:54 +0200

    182. Fetching All Products

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ const bodyParser = require("body-parser");
 const pageNotFoundController = require("./controllers/404");
 const mongoConnect = require("./utils/database").mongoConnect;
 
-
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
-// const shopRoutes = require("./routes/shop");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -29,13 +28,8 @@ app.use((req, res, next) => {
 });
 
 app.use("/admin", adminRoutes);
-// app.use(shopRoutes);
+app.use(shopRoutes);
 
 app.use(pageNotFoundController);
 
-mongoConnect(client => {
-  console.log(client);
-  app.listen(3000);
-})
-
-
+mongoConnect(() => app.listen(3000));
