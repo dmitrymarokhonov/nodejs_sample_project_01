79e3bcd787b3e4fa2667215f198e3c8e54876a7d
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2019-02-06 13:51:57 +0200

    183 Fetching Single Product

--- utils/database.js
+++ utils/database.js
@@ -5,7 +5,8 @@ let _db;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    "mongodb+srv://dmitry:KUyt17sX88iDUKnM@cluster0-qvwe4.mongodb.net/shop?retryWrites=true"
+    "mongodb+srv://dmitry:KUyt17sX88iDUKnM@cluster0-qvwe4.mongodb.net/shop?retryWrites=true",
+    { useNewUrlParser: true }
   )
     .then(client => {
       console.log("Connected!");
@@ -19,12 +20,12 @@ const mongoConnect = callback => {
 };
 
 const getDb = () => {
-  if(_db) {
+  if (_db) {
     return _db;
   } else {
     throw "No database found!";
   }
-}
+};
 
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
