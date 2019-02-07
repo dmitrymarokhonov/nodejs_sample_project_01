937ae7e64dfaa8f2f07ec80a95fa5fe0b7f0ff1c
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2019-02-07 13:53:40 +0200

    189 Fixing the the "Add Product" Functionality. Editin & Deleting added

--- models/product.js
+++ models/product.js
@@ -2,20 +2,24 @@ const mongodb = require("mongodb");
 const getDb = require("../utils/database").getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
-      .then(result => console.log(result))
-      .catch(err => console.log(err));
+    let dbOp;
+    if (this._id) {
+      // Update the product
+      dbOp = db.collection("products").updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOp = db.collection("products").insertOne(this);
+    }
+    return dbOp.then(result => console.log(result)).catch(err => console.log(err));
   }
 
   static fetchAll() {
@@ -28,7 +32,9 @@ class Product {
         console.log(products);
         return products;
       })
-      .catch(err => console.log(err));
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   static findById(prodId) {
@@ -41,6 +47,17 @@ class Product {
         console.log(product);
         return product;
       })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then(result => console.log(`deleted ${prodId}`))
       .catch(err => console.log(err));
   }
 }
