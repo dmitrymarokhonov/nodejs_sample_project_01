025911d3c45d02617510a86f3efdece7c2f994a6
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2019-02-08 07:40:53 +0200

    193 Adding the "Add to Cart" Functionality 1:44

--- models/user.js
+++ models/user.js
@@ -1,15 +1,31 @@
 const mongodb = require("mongodb");
 const getDb = require("../utils/database").getDb;
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     let dbOp = db.collection("users").insertOne(this);
     return dbOp.then(result => console.log(result)).catch(err => console.log(err));
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id;
+    // })
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
