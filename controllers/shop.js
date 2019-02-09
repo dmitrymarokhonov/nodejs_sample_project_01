1d2e0a3ccc46f0db9bc2928a0880701cf65440bb
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2019-02-09 12:53:34 +0200

    193 Adding the "Add to Cart" Functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -58,6 +58,13 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
+  Product.findById(prodId)
+    .then(product => {
+      return req.user.addToCart(product);
+    })
+    .then(result => {
+      console.log(result);
+    });
   // let fetchedCart;
   // let newQuantity = 1;
   // req.user
@@ -135,7 +142,7 @@ exports.postOrder = (req, res, next) => {
 
 exports.getOrders = (req, res, next) => {
   req.user
-    .getOrders({include: ["products"]})
+    .getOrders({ include: ["products"] })
     .then(orders => {
       res.render("shop/orders", {
         docTitle: "Orders",
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongodb = require("mongodb");
 const getDb = require("../utils/database").getDb;
+const ObjectId = mongodb.ObjectID;
 class User {
   constructor(username, email, cart, id) {
     this.name = username;
@@ -18,12 +19,12 @@ class User {
     // const cartProduct = this.cart.items.findIndex(cp => {
     //   return cp._id === product._id;
     // })
-    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const updatedCart = { items: [{ productId: new ObjectId(product._id), quantity: 1 }] };
     const db = getDb();
     return db
       .collection("users")
       .updateOne(
-        { _id: new mongodb.ObjectId(this._id) },
+        { _id: new ObjectId(this._id) },
         { $set: { cart: updatedCart } }
       );
   }
