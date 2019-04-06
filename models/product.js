8dcdf180454e57575988be56d33341ae6d2a8155
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2019-04-07 01:21:37 +0300

    215: Using relations in Mongoose

--- models/product.js
+++ models/product.js
@@ -18,6 +18,11 @@ const productSchema = new Schema({
   imageUrl: {
     type: String,
     required: true
+  },
+  userId : {
+    type: Schema.Types.ObjectId,
+    ref: "User", // refers to ./user (src/models/user.js)
+    required: true
   }
 })
 
