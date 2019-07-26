2cc9e1e1bbcdd1ba320e0c063d7e3b0f8362ccbb
Dmitry Marokhonov <Dmitry.Marokhonov@cybercom.com>
2019-07-26 15:26:05 +0300

    192 Storing the User in Our Database

--- app.js
+++ app.js
@@ -4,30 +4,31 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 app.set('views', 'views');
 
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
+  User.findById("5d3ae0de1c9d440000ce939f")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
-// app.use('/admin', adminRoutes);
-// app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
--- models/user.js
+++ models/user.js
@@ -1,16 +1,35 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
 
-const sequelize = require('../util/database');
+const ObjectId = mongodb.ObjectId;
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+    // this._id = id ? ObjectId(id) : null;
+  }
+  save() {
+    const db = getdDb();
+    let dbOp;
+    if (this._id) {
+      // dbOp = db.collection("users").updateOne({_id: this._id}, {$set: this});
+    } else {
+      dbOp = db.collection("users").insertOne(this);
+    }
+    return dbOp.then(res => console.log(res)).catch(err => console.log(err));
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new ObjectId(userId) })
+      .then(user => {
+        console.log(user);
+        return user;
+      })
+      .catch(err => console.log(err));
+  }
+}
 
 module.exports = User;
