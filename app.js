94e2c9e962697ebbc6664d1c6532dd7f9116979f
Dmitry Marokhonov <Dmitry.Marokhonov@cybercom.com>
2019-07-31 18:09:31 +0300

    238 Using MongoDB to Store Sessions

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const favicon = require("serve-favicon");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongodbStore = require("connect-mongodb-session")(session);
 
 const pageNotFoundController = require("./controllers/404");
 const User = require("./models/user");
 
+const MONGODB_URI =
+  "mongodb+srv://dmitry:OvOTvIZHoxySg5PN@cluster0-qvwe4.mongodb.net/shop";
+// const MONGODB_URI =
+//   "mongodb+srv://dmitry:OvOTvIZHoxySg5PN@cluster0-qvwe4.mongodb.net/shop?retryWrites=true&w=majority";
+
 const app = express();
+const store = new MongodbStore({
+  uri: MONGODB_URI,
+  collection: "sessions"
+});
 
 app.set("view engine", "pug");
 app.set("views", "views");
@@ -19,9 +30,12 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({ secret: "my secret", resave: false, saveUninitialized: false, store: store })
+);
 
 app.use((req, res, next) => {
-  User.findById('5d4049153b902028c05b7ffe')
+  User.findById("5d4049153b902028c05b7ffe")
     .then(user => {
       req.user = user;
       next();
@@ -32,15 +46,12 @@ app.use((req, res, next) => {
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
-app.use(favicon(path.join(__dirname, 'images', 'icons8-laptop-50.png')));
+app.use(favicon(path.join(__dirname, "images", "icons8-laptop-50.png")));
 
 app.use(pageNotFoundController);
 
 mongoose
-  .connect(
-    "mongodb+srv://dmitry:OvOTvIZHoxySg5PN@cluster0-qvwe4.mongodb.net/shop?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
+  .connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
     User.findOne().then(user => {
       if (!user) {
