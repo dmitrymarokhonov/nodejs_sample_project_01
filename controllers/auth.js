3d9b8d255f2fb2230f5c896faebd839048a36779
Dmitry Marokhonov <Dmitry.Marokhonov@cybercom.com>
2019-08-08 15:51:25 +0300

    297 Adding Validation to Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -27,7 +27,9 @@ exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
     docTitle: "Login",
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {email: "", password: ""},
+    validationErrors: []
   });
 };
 
@@ -56,14 +58,21 @@ exports.postLogin = (req, res, next) => {
     return res.status(422).render("auth/login", {
       path: "/login",
       docTitle: "Login",
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: { email: email, password: password },
+      validationErrors: errors.array()
     });
   }
   User.findOne({ email: email })
     .then(user => {
       if (!user) {
-        req.flash("error", "Invalid email or password.");
-        return res.redirect("/login");
+        return res.status(422).render("auth/login", {
+          path: "/login",
+          docTitle: "Login",
+          errorMessage: "Invalid email or password.",
+          oldInput: { email: email, password: password },
+          validationErrors: []
+        });
       }
       bcrypt
         .compare(password, user.password)
@@ -76,8 +85,13 @@ exports.postLogin = (req, res, next) => {
               res.redirect("/");
             });
           }
-          req.flash("error", "Invalid email or password.");
-          res.redirect("/login");
+          return res.status(422).render("auth/login", {
+            path: "/login",
+            docTitle: "Login",
+            errorMessage: "Invalid email or password.",
+            oldInput: { email: email, password: password },
+            validationErrors: []
+          });
         })
         .catch(err => {
           console.log(err);
