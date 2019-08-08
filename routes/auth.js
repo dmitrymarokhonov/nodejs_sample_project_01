edfeabbac1da8faf54600b6e3e0b3b2450eb8e66
Dmitry Marokhonov <Dmitry.Marokhonov@cybercom.com>
2019-08-08 09:59:30 +0300

    293 Checking For Field Equality

--- routes/auth.js
+++ routes/auth.js
@@ -26,7 +26,13 @@ router.post(
       "Please enter a password with only numbers and text and at least 5 characters."
     )
       .isLength({ min: 5 })
-      .isAlphanumeric()
+      .isAlphanumeric(),
+    body("confirmPassword").custom((val, { req }) => {
+      if (val !== req.body.password) {
+        throw new Error("Passwords have to match!");
+      }
+      return true;
+    })
   ],
   authController.postSignup
 );
