9f1fef15e0a6235e7e8c07ec42474f08c84efb94
Dmitry Marokhonov <Dmitry.Marokhonov@cybercom.com>
2019-08-12 14:45:13 +0300

    325 Downloading Files with Authentication

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const Product = require("../models/product");
 const Order = require("../models/order");
 
@@ -156,4 +159,16 @@ exports.getOrders = (req, res, next) => {
       console.log(error);
       return next(error);
     });
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId; // hint routes: router.get("/orders/:orderId", isAuth, shopController.getInvoice);
+  const invoiceName = "invoice-" + orderId + ".pdf";
+  const invoicePath = path.join("data", "invoices", invoiceName);
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+    res.send(data);
+  });
 };
