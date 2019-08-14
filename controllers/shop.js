9dd6f21ecfae24c6c3e65871b9ea8384be20d943
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2019-08-15 00:13:51 +0300

    353 Adding a Checkout Page

--- controllers/shop.js
+++ controllers/shop.js
@@ -145,6 +145,31 @@ exports.postCartDeleteProduct = (req, res, next) => {
     });
 };
 
+exports.getCheckout = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items;
+      let total = 0;
+      products.forEach(p => {
+        total += p.quantity * p.productId.price;
+      })
+      res.render("shop/checkout", {
+        docTitle: "Checkout",
+        path: "/checkout",
+        products: products,
+        totalSum: total
+      });
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      console.log(error);
+      return next(error);
+    });
+}
+
 exports.postOrder = (req, res, next) => {
   req.user
     .populate("cart.items.productId")
